2113b1617d9f04f3081631a8bd0ba186b48f83f8
Dor <Dor.Shaar@votiro.com>
2021-09-10 07:50:48 +0300

    Implement dynamic menu

--- docker-run.bat
+++ docker-run.bat
@@ -4,4 +4,4 @@ echo all versions:
 docker images | grep tasker-planner | awk '{print $2}' | sort
 
 set /p id="Enter version to run: "
-docker run -it -v /c/Dor/Apps/TaskerPlanner:/app/plan/ tasker-planner:%id%
+docker run -it -v /c/Dor/Apps/TaskerPlanner:/app/data tasker-planner:%id%
--- package/taskerPlanner.py
+++ package/taskerPlanner.py
@@ -1,30 +1,72 @@
 import logging
 import sys
+import os
 from consolemenu import *
 from consolemenu.items import *
+from consolemenu.format import *
 from domain.stringReplacer import StringReplacer
 from infra.userQuestioner import UserQuestioner
 
-def main():
+PLANS_DIRECTORY = "data/plans/"
+# PLANS_DIRECTORY = "C:/Dor/Apps/TaskerPlanner/plans"
+PLAN_QUESTIONS_FILE_PATH = "data/plan-questions/plan.json"
+
+def setLogger():
     logging.basicConfig(filename='taskerPlanner.log', level=logging.DEBUG)
     # format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     streamHandler = logging.StreamHandler(sys.stdout)
     # streamHandler.setFormatter(format)
     logging.getLogger().addHandler(streamHandler)
 
-    stringReplacer = StringReplacer()
-    userQuestioner = UserQuestioner(stringReplacer)
+def getPlansList():
+    return os.listdir(PLANS_DIRECTORY)
 
-    # Create the menu
+def createChoosePlanItem() -> SubmenuItem:
+    showPlansItem = SelectionMenu(getPlansList())
+    return SubmenuItem("Show all plans", showPlansItem)
+
+def createMenu(userQuestioner: UserQuestioner) -> ConsoleMenu:
     menu = ConsoleMenu("Tasker Planner", "By dorshaar")
+    menu.formatter = MenuFormatBuilder().set_title_align('center').set_subtitle_align('center').set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_BORDER).show_prologue_top_border(True).show_prologue_bottom_border(True)
 
-    askQuestionsFromJsonFileItem = FunctionItem("Start new plan", userQuestioner.askQuestionsFromJsonFile, ['plan/plan.json'])
+    askQuestionsFromJsonFileItem = FunctionItem("Start new plan", userQuestioner.askQuestionsFromJsonFile, [PLAN_QUESTIONS_FILE_PATH])
     editPlanItem = FunctionItem("Edit exiting plan", userQuestioner.editPlan)
+    choosePlanItem = createChoosePlanItem()
 
     menu.append_item(askQuestionsFromJsonFileItem)
     menu.append_item(editPlanItem)
+    menu.append_item(choosePlanItem)
+    menu.add_exit()
 
-    menu.show()
+    return menu
+
+def runMenu(menu: ConsoleMenu):
+    currentPlan = "None"
+
+    while not menu.is_selected_item_exit():
+        menu.prologue_text = f"Current plan: {currentPlan}"
+        
+        if menu.selected_item is menu.items[2]:
+            plansList = getPlansList()
+            if menu.items[2].get_return() is not None:
+                currentPlan = plansList[menu.items[2].get_return()]
+                menu.prologue_text = f"Current plan: {currentPlan}"
+
+        choosePlanItem = createChoosePlanItem()
+        menu.remove_item(menu.items[2])
+        menu.append_item(choosePlanItem)
+
+        menu.draw()
+        menu.process_user_input()
+
+def main():
+    setLogger()
+
+    stringReplacer = StringReplacer()
+    userQuestioner = UserQuestioner(stringReplacer)
+
+    menu = createMenu(userQuestioner)
+    runMenu(menu)
 
     logging.info('Tasker Planner Finished')
 
